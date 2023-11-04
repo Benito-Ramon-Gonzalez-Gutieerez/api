a552964bfec1ded9b97c47fcf4de8fd296436703
Benito <you@example.com>
2023-11-04 16:26:38 -0600

    mejorar rutas

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-
   get '/index', to: 'api#index'
 
-  get '/principal', to: 'api#principal'
+  root 'api#principal'
 
   # Defines the root path route ("/")
   # root "posts#index"
