6877c13b93238da9280d59ef6e6289e295823843
Isaac <maripanadillo@gmail.com>
2023-11-04 15:35:57 -0600

    advance add api nasa proyect

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,12 @@
 class ApiController < ApplicationController
-
-def index
+    require 'net/http'
+    require 'json'
     
-
+def index
+    url = URI("https://api.nasa.gov/planetary/apod?api_key=bR0mRUfPzms4xK8omhWSVHVRJZ4ALEEtTptOkBxe")
+    response = Net::HTTP.get(url)
+    data = JSON.parse(response)
+    @apod_data = data
 end
 
 end
